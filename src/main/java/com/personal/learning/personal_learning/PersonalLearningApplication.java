3d1e08883aa291b54c99116c49f5f3fa2500bf1d
unknown <khu0mmh@gmail.com>
2025-05-14 06:26:12 +0700

    first commit

---
+++ src/main/java/com/personal/learning/personal_learning/PersonalLearningApplication.java
@@ -0,0 +1,13 @@
+package com.personal.learning.personal_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/learning/personal_learning/PersonalLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.learning.personal_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
